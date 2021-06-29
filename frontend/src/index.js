6f76ab61084becf42a558f3229f78aba693bd29f
Steph <stephanie.ohn@gmail.com>
2021-06-29 15:59:22 -0400

    defines store using createStore + passes to Provider comp as props + imports compose, applyMiddleware, composeWithDevTools, and thunk + defines initialState to also pass to createStore

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
+import {createStore, compose, applyMiddleware} from 'redux'
+import notesReducer from './notesReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
 
+const initialState = {notes: []}
+const store = createStore(notesReducer, initialState, compose(applyMiddleware(thunk), composeWithDevTools()))
 
 ReactDOM.render(
   <Router>
-    <Provider>
+    <Provider store={store}>
       <App />
       </Provider>
   </Router>,
