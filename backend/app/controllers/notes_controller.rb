85774e2b3e792c89407f78507257bdcefeade2c4
Steph <stephanie.ohn@gmail.com>
2021-06-29 14:22:38 -0400

    sets up show action

--- backend/app/controllers/notes_controller.rb
+++ backend/app/controllers/notes_controller.rb
@@ -4,4 +4,9 @@ class NotesController < ApplicationController
         render json: Note.all
     end
 
+    def show
+        note = Note.find_by(id: params[:id])
+        render json: note
+    end
+
 end
