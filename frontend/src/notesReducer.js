4f991216328e15a422841a72be46ecdaf3547377
Steph <stephanie.ohn@gmail.com>
2021-06-30 17:40:24 -0400

    adds case to reducer fn

--- frontend/src/notesReducer.js
+++ frontend/src/notesReducer.js
@@ -1,5 +1,7 @@
 export default function notesReducer(state = {notes: []}, action) {
     switch (action.type) {
+        case "ADD_NOTE":
+            return {notes: [...state.notes, action.payload]}
         case "FETCH_NOTES":
             return {notes: action.payload}
         default:
